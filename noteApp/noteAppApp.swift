4e9ccd859f242344d107c61f56c239023532a45f
Nathan Gomez <pgomez@progrexion.com>
2024-02-15 15:05:00 -0500

    Initial Commit

---
+++ noteApp/noteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteAppApp.swift
+//  noteApp
+//
+//  Created by Pablo Nathanielle Gomez on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct noteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
